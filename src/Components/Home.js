ea67967766197838ca0189613c1ad61b6c18f622
Aditya Nandan <aditya28nandan@gmail.com>
2022-01-05 09:10:38 +0530

    navbar responsive completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import "./css/header.css"
-import Services from './Services';
-import Advisers from './Advisers';
+import React from "react";
+import "./css/header.css";
+import Services from "./Services";
+import Advisers from "./Advisers";
 // import Startup from './Startup';
-import ContactUs from './ContactUs';
+import ContactUs from "./ContactUs";
 
 function Home() {
-    return (
-        <>
-      
-     
-       <section className="head">
-            <div className="cont">
-                <h1 className="contH1">A One Stop place for <strong>Individuals,Founders,Co-Founders </strong>to kick start their StartUp</h1>
-                <button className="contBtn"> Get Started Now</button>
-            </div>
-        </section>
-        <Services />
-       
-        <Advisers />
-        {/* <Startup /> */}
-       
-        </>
-    )
+  return (
+    <>
+      <section className="head">
+        <div className="cont">
+          <h1 className="contH1">
+            A One Stop place for{" "}
+            <strong>Individuals,Founders,Co-Founders </strong>to kick start
+            their StartUp
+          </h1>
+          <button className="contBtn"> Get Started Now</button>
+        </div>
+      </section>
+      <Services />
+
+      <Advisers />
+      {/* <Startup /> */}
+    </>
+  );
 }
 
 export default Home;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,31 +1,63 @@
-import React from 'react'
-import "./css/navbar.css"
-import {Link} from "react-router-dom";
-import logo from "./images/logo.png"
-import hamburger from "./images/hamburger.png"
+import React, { useState } from "react";
+import "./css/navbar.css";
+import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
+import hamburger from "./images/hamburger.png";
 
 function NavBar() {
-    return (
-        <>
- <nav>
+  const [checked, setCheck] = useState(false);
+  function handleCheck() {
+    if (checked) {
+      setCheck(false);
+    } else setCheck(true);
+    console.log(checked);
+  }
+  return (
+    <>
+      <nav>
         <div className="logo">
-            <div className="imgLogo">
-            <Link to="/"><img className="image1" src={logo}  alt="sdcsdcfd" /></Link>
-            </div>
-            <Link to="/"><img className="hamburger" src={hamburger}  alt="hamburger" /></Link>
+          <div className="imgLogo">
+            <Link to="/">
+              <img className="image1" src={logo} alt="sdcsdcfd" />
+            </Link>
+          </div>
+          <input type="checkbox" value={checked} id="openNavbar" />
+          <label htmlFor="openNavbar" className="toggleBar">
+            <img
+              className="hamburger"
+              src={hamburger}
+              alt="hamburger"
+              onClick={handleCheck}
+            />
+          </label>
         </div>
-        <div className="listItem">
-            <ul>
-                <Link to="/"><li>Home</li></Link>
-                <Link to="/services"><li>Services</li></Link>
-                <Link to="/contact"><li>Contact</li></Link>
-                <Link to="/login"><li><button className="btn1">Login</button></li></Link>
-               <Link to="/signup"> <li><button className="btn2">Signup</button></li></Link>
-            </ul>
+        <div className={checked ? "listItem" : "handleListItem1"}>
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/services">
+              <li>Services</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact</li>
+            </Link>
+            <Link to="/login">
+              <li>
+                <button className="btn1">Login</button>
+              </li>
+            </Link>
+            <Link to="/signup">
+              {" "}
+              <li>
+                <button className="btn2">Signup</button>
+              </li>
+            </Link>
+          </ul>
         </div>
-    </nav>
-        </>
-    )
+      </nav>
+    </>
+  );
 }
 
 export default NavBar;
